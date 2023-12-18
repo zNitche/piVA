07b71a43a93559d8115b1763fb35bbd1a34565eb
zNitche <53448802+zNitche@users.noreply.github.com>
2023-12-18 20:21:51 +0100

    espeak wrapper + integration

--- piva/modules/objects_detection_module.py
+++ piva/modules/objects_detection_module.py
@@ -10,15 +10,36 @@ class ObjectsDetectionModule(ModuleBase):
 
     @staticmethod
     def get_dependencies():
-        return [Dependencies.CAMERA_REQUIRED]
+        return [Dependencies.CAMERA_REQUIRED, Dependencies.SPEECH_SYNTHESIZER_REQUIRED]
 
     def process(self, *args, **kwargs):
-        camera = kwargs["camera"]
+        camera = kwargs.get("camera")
+        speech_synthesizer = kwargs.get("speech_synthesizer")
 
-        if camera:
+        if camera and speech_synthesizer:
             image = camera.capture_image("main")
 
             if image:
                 results = detect.process(image)
+                classes_count = self.__count_classed(results)
 
-                print(results)
+                speech_synthesizer.say(self.__generate_text_for_results(classes_count))
+
+    def __generate_text_for_results(self, classes):
+        text = ""
+
+        for cl in classes:
+            text += f"{classes[cl]} {cl}, "
+
+        return "no objects has been found" if len(classes.keys()) == 0 else text
+
+    def __count_classed(self, results):
+        counted_classed = {}
+
+        for cl in results:
+            if cl["class"] not in counted_classed.keys():
+                counted_classed[cl["class"]] = 0
+
+            counted_classed[cl["class"]] += 1
+
+        return counted_classed
--- piva/piva.py
+++ piva/piva.py
@@ -1,12 +1,17 @@
 import RPi.GPIO as GPIO
 from picamera2 import Picamera2
+import alsaaudio
 from piva.config import Config
+from piva.speech_synthesizer.synthesizer import SpeechSynthesizer
 from piva.modules.dependencies import Dependencies as ModulesDependencies
 
 
 class PiVA:
     def __init__(self, debug=False):
         self.camera = None
+        self.audio_mixer = None
+        self.speech_synthesizer = None
+
         self.modules = []
         self.current_mode = None
 
@@ -19,12 +24,21 @@ class PiVA:
         if not self.initialized:
             self.__log("PiVA setup started...")
 
+            self.__init_audio()
             self.__init_camera()
             self.__setup_buttons()
             self.__setup_modules()
 
             self.initialized = True
             self.__log("PiVA setup completed...")
+
+    def __init_audio(self):
+        self.__log("Audio init...")
+
+        self.audio_mixer = alsaaudio.Mixer("PCM")
+        self.speech_synthesizer = SpeechSynthesizer()
+
+        self.__log("Audio ready...")
 
     def __init_camera(self):
         self.__log("Camera init...")
@@ -41,13 +55,16 @@ class PiVA:
     def __setup_buttons(self):
         self.__log("Buttons init...")
         GPIO.setup(Config.ACCEPT_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(Config.ACCEPT_BUTTON_PIN, GPIO.RISING, callback=self.__button_pressed_callback, bouncetime=1000)
+        GPIO.add_event_detect(Config.ACCEPT_BUTTON_PIN, GPIO.RISING, callback=self.__button_pressed_callback,
+                              bouncetime=1500)
 
         GPIO.setup(Config.UP_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(Config.UP_BUTTON_PIN, GPIO.RISING, callback=self.__button_pressed_callback, bouncetime=1000)
+        GPIO.add_event_detect(Config.UP_BUTTON_PIN, GPIO.RISING, callback=self.__button_pressed_callback,
+                              bouncetime=1500)
 
         GPIO.setup(Config.DOWN_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(Config.DOWN_BUTTON_PIN, GPIO.RISING, callback=self.__button_pressed_callback, bouncetime=1000)
+        GPIO.add_event_detect(Config.DOWN_BUTTON_PIN, GPIO.RISING, callback=self.__button_pressed_callback,
+                              bouncetime=1500)
 
         self.__log("Buttons ready...")
 
@@ -55,8 +72,10 @@ class PiVA:
         self.__log("Modules init...")
 
         from piva.modules.objects_detection_module import ObjectsDetectionModule
+        from piva.modules.volume_up_module import VolumeUpModule
+        from piva.modules.volume_down_module import VolumeDownModule
 
-        self.modules = [ObjectsDetectionModule()]
+        self.modules = [ObjectsDetectionModule(), VolumeUpModule(), VolumeDownModule()]
         self.current_mode = self.modules[0]
 
         self.__log("Modules ready...")
@@ -83,21 +102,40 @@ class PiVA:
     def __execute_current_module(self):
         if self.current_mode:
             self.__log(f"Executing {self.current_mode.get_module_name()}...")
+            self.speech_synthesizer.say(f"Executing {self.current_mode.get_module_name()}")
 
             self.processing = True
 
-            args = {
-                "camera": self.camera if ModulesDependencies.CAMERA_REQUIRED in self.current_mode.get_dependencies() else None
-            }
-
-            self.current_mode.process(**args)
+            self.current_mode.process(**self.__resolve_module_dependencies(self.current_mode))
             self.processing = False
 
+    def __resolve_module_dependencies(self, module):
+        dependencies = {
+            "camera": self.__get_dependency(ModulesDependencies.CAMERA_REQUIRED, module, self.camera),
+            "audio_mixer": self.__get_dependency(ModulesDependencies.AUDIO_MIXER_REQUIRED, module, self.audio_mixer),
+            "speech_synthesizer": self.__get_dependency(ModulesDependencies.SPEECH_SYNTHESIZER_REQUIRED, module, self.speech_synthesizer)
+        }
+
+        return dependencies
+
+    def __get_dependency(self, dependency, module, return_value):
+        return return_value if dependency in module.get_dependencies() else None
+
     def __switch_to_prev_module(self):
-        pass
+        current_module_id = self.modules.index(self.current_mode)
+
+        self.current_mode = self.modules[len(self.modules) - 1] if current_module_id - 1 < 0 \
+            else self.modules[current_module_id - 1]
+
+        self.speech_synthesizer.say(f"Current mode {self.current_mode.get_module_name()}")
 
     def __switch_to_next_module(self):
-        pass
+        current_module_id = self.modules.index(self.current_mode)
+
+        self.current_mode = self.modules[0] if current_module_id + 1 >= len(self.modules) \
+            else self.modules[current_module_id + 1]
+
+        self.speech_synthesizer.say(f"Current mode {self.current_mode.get_module_name()}")
 
     def __log(self, message):
         if self.debug:
